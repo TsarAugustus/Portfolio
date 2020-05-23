57548930b7bf81fb04860791c7a3dd1aee05e85b
John Ostap <ostap.john@gmail.com>
2020-05-23 02:01:12 -0700

    Neat transition in progress

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,7 +1,8 @@
-console.log('yo');
-
+console.log('Main.js')
 
 function main() {
   console.log('loaded');
   document.getElementById("img").classList.add('load');
+  document.getElementById("intro").classList.add('textLoad');
+  // setTimeout(textLoad, 2000);
 }
