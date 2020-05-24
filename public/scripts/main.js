6736f99e5ac3601cf7b1b8f59175250ab3323bb7
John Ostap <ostap.john@gmail.com>
2020-05-23 18:33:58 -0700

    Animation on click, slowly refactoring code

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,8 +1,19 @@
 console.log('Main.js')
+const landingLink = document.getElementById('landingLink');
+const aboutLink = document.getElementById('aboutLink');
+const landing = document.getElementById('landing');
+
+
+landingLink.onclick = function() {
+  landing.classList.add('showAnimation');
+}
+
+aboutLink.onclick = function() {
+  console.log('about')
+}
+
 
 function main() {
   console.log('loaded');
-  document.getElementById("img").classList.add('load');
-  document.getElementById("intro").classList.add('textLoad');
-  // setTimeout(textLoad, 2000);
+  // document.getElementById("landing").classList.add('showAnimation');
 }
